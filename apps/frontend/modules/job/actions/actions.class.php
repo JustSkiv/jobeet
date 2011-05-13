9c6467983ef5750922db5a32000c488873415c6a
Skiv <just.skiv@gmail.com>
2011-05-14 02:43:13 +0600

    day 6, 20%

--- apps/frontend/modules/job/actions/actions.class.php
+++ apps/frontend/modules/job/actions/actions.class.php
@@ -12,10 +12,7 @@ class jobActions extends sfActions
 {
   public function executeIndex(sfWebRequest $request)
   {
-    $this->jobeet_jobs = Doctrine_Core::getTable('JobeetJob')
-      ->createQuery('a')
-      ->execute();
-    $this->myvar = "test_var_skiv";
+    $this->categories = Doctrine_Core::getTable('JobeetCategory')->getWithJobs();
   }
 
   public function executeShow(sfWebRequest $request)
@@ -70,11 +67,10 @@ class jobActions extends sfActions
   protected function processForm(sfWebRequest $request, sfForm $form)
   {
     $form->bind($request->getParameter($form->getName()), $request->getFiles($form->getName()));
-    if ($form->isValid())
-    {
+    if ($form->isValid()) {
       $jobeet_job = $form->save();
 
-      $this->redirect('job/edit?id='.$jobeet_job->getId());
+      $this->redirect('job/edit?id=' . $jobeet_job->getId());
     }
   }
 }
